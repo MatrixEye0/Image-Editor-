e821133c99d336839556985432ae6f3f1a3490f2
matrixeye0 <singhjagjit9735@gmail.com>
2026-08-09 00:39:34 +0530

    create left section

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Image-Editor-
-build image editor webpage using html,  CSS,  JS for practice
+# build image editor webpage using html,  CSS,  JS for practice
+
+            <p>Contrast</p> 
+            <input name="Contrast" value="50" type="range" min="0" max="100">
+             <p>Exposure</p>
+            <input name="Exposure" value="50" type="range" min="0" max="100">
+             <p>Saturation</p>
+            <input name="Saturation" value="50" type="range" min="0" max="100">
+             <p>Hue Rotate</p>
+            <input name="Hue-Rotate" value="0" type="range" min="0" max="100">
+             <p>Blur</p>
+            <input name="Blur" value="0" type="range" min="0" max="100">
+             <p>Gray Scale</p>
+            <input name="Gray-Scale" value="0" type="range" min="0" max="100">
+             <p>sepia</p>
+            <input name="Sepia" value="0" type="range" min="0" max="100">
+             <p>Opacity</p>
+            <input name="Opacity" value="0" type="range" min="0" max="100">
+             <p>Invert</p>
+            <input name="Invert" value="0" type="range" min="0" max="100">
---
+++ index.js
@@ -0,0 +1,87 @@
+const filters = {
+    brightness:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    contrast:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    exposers:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    saturation:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    huerotate: {
+        value:100,
+        min:0,
+        max:360,
+        unit:"deg"
+    },
+    blur:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"px"
+    },
+    grayscale:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    sepia:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    opacity:{
+        value:100,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+    invert:{
+        value:0,
+        min:0,
+        max:100,
+        unit:"%"
+    },
+}
+
+const filtersContainer=document.querySelector('.filters')
+
+function createfilter(name,unit="%", value,min,max){
+   const div= document.createElement('div')
+   div.classList.add('filter')
+   const input=document.createElement('input')
+   input.type='range'
+   input.min=min
+   input.max=max
+   input.value=value
+   //input.unit=unit
+   input.id=name
+
+   const p= document.createElement('p')
+   p.innerText=name
+
+   div.appendChild(p)
+   div.appendChild(input)
+}
+
+Object.keys(filters).forEach(key=>{
+  const filterElement = createfilter( key, filters[key].unit,filters[key].value,filters[key].min,filters[key].max)
+  filtersContainer.appendChild(filterElement)
+})
